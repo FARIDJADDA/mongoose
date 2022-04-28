6e187620832ee64cb38ba29fcd00fbf216e202ee
FARIDJADDA <faridjadda750@gmail.com>
2022-04-28 15:32:04 +0200

    introduce schemas

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const mongoose = require("mongoose");
+const schema = mongoose.Schema;
+
+const projectSchema = schema({
+  title: String,
+  index: Number,
+  nbrOfLessons: Number,
+  active: Boolean,
+});
+
+const Projects = mongoose.model("chapters", projectSchema);
 
 mongoose
-  .connect("mongodb://alex:qwe@localhost:27017/cpstudio92", {
+  .connect("mongodb://alex:qwe@localhost:27017/cpstudiodb", {
     useNewUrlParser: true,
   })
   .then(() => {
     console.log("ok");
+    Projects.find({}, (err, documents) => {
+      console.log(documents);
+      // console.log(err);
+    });
   })
   .catch((err) => {
     console.log(err);
